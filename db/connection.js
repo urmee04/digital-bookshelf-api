6f9a1b8f46e9f71c90be014844574f6fd3d87271
urmee04 <urmee04sust@gmail.com>
2025-08-24 09:23:58 -0400

    Updated db/connection.js to throw errors instead of exiting process

--- db/connection.js
+++ db/connection.js
@@ -5,16 +5,14 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 //async function to connect to MongoDB
-
 async function connectDatabase() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("Connection Established");
   } catch (error) {
     console.error("Connection failed:", error.message);
-    //exit process if connection fails
-    process.exit(1);
+    //rethrow error so server.js can handle process exit
+    throw error;
   }
 }
 
